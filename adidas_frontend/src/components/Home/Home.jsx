1cd55d9f2ebde31fcd3b78c2be74752f20a1ac0a
Chetan2380 <chetaningale2398@gmail.com>
2025-05-18 10:44:25 +0530

    Update

--- adidas_frontend/src/components/Home/Home.jsx
+++ adidas_frontend/src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import "../Home/Home.css"
+import { HiArrowLongRight } from "react-icons/hi2";
 
 const Home = () => {
   return (
@@ -8,6 +9,36 @@ const Home = () => {
         <Navbar/>
         <div className='findyoursummerlook'>
             <img src='https://brand.assets.adidas.com/image/upload/f_auto,q_auto:best,fl_lossy/if_w_gt_1920,w_1920/HP_MH_1_10ebaf5c6d.jpg'/>
+            <div id="summercolltext1">
+              <p>SUMMER COLLECTION</p>
+            </div>
+            <div id="summercolltext2">
+              <p>Amp Your Style - It's Time to Shine</p>
+            </div>
+            <div id="summercollshopnow">
+              <div><p>SHOP NOW</p></div> <div><HiArrowLongRight  style={{color: "black", fontSize: "26px", fontWeight:"500px"}}/></div>
+            </div>
+            <div id="summercollshopnow2"></div>
+        </div>
+
+        <div id="shopbestofsum">
+          <div id="shopbestofsumtext1">
+            <p>Shop Best of Summer</p>
+          </div>
+          <div id="shopbestofsumimgs">
+            <div class="shopbestofsumimg-wrapper">
+              <div class="shopbestofsumimg" id="shopbestofsumimg1"></div>
+              <div class="shopbestofsumimgtext1">Shorts</div>
+            </div>
+            <div class="shopbestofsumimg-wrapper">
+              <div class="shopbestofsumimg" id="shopbestofsumimg2"></div>
+              <div class="shopbestofsumimgtext2">T-Shirts</div>
+            </div>
+            <div class="shopbestofsumimg-wrapper">
+              <div class="shopbestofsumimg" id="shopbestofsumimg3"></div>
+              <div class="shopbestofsumimgtext3">Slides</div>
+            </div>
+          </div>
         </div>
     </div>
   )
