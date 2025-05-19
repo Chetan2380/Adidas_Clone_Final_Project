870fc7fa67cc20bec328bb0b217f301e6a3391d9
Chetan2380 <chetaningale2398@gmail.com>
2025-05-19 11:09:24 +0530

    Update

--- adidas_frontend/src/components/Home/Home.jsx
+++ adidas_frontend/src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import "../Home/Home.css"
 import { HiArrowLongRight } from "react-icons/hi2";
 import { PiHeartStraight } from "react-icons/pi";
 import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+import { FaInstagram } from "react-icons/fa";
 
 const ITEMS_PER_PAGE = 4;
 const cardWidth = 330; // width of one card
@@ -305,6 +306,72 @@ const [currentPage, setCurrentPage] = useState(0);
             <div><p>SIGN UP FOR FREE</p></div> <div><HiArrowLongRight  style={{color: "white", fontSize: "26px", fontWeight:"500px"}}/></div>
           </div>
           <div id="signupforfree2"></div>
+        </div>
+
+        <div className='footer'>
+          <div className='footer1'>
+            <div className='products'>
+              <p>PRODUCTS</p>
+              <p>Footwear</p>
+              <p>Clothing</p>
+              <p>Accessories</p>
+              <p>Outlet-Sale</p>
+              <p>New Arrivals</p>
+              <p>Flat 50% Off!</p>
+            </div>
+            <div className='sports'>
+              <p>SPORTS</p>
+              <p>CRICKET</p>
+              <p>Running</p>
+              <p>Football</p>
+              <p>Gym/Training</p>
+              <p>Tennis</p>
+              <p>Outdoor</p>
+              <p>Basketball</p>
+              <p>Swimming</p>
+              <p>Skateboarding</p>
+              <p>Motosport</p>
+            </div>
+            <div className='collections'>
+              <p>COLLECTIONS</p>
+              <p>Ultraboost</p>
+              <p>Superstar</p>
+              <p>NMD</p>
+              <p>Stan Smith</p>
+              <p>Sustainability</p>
+              <p>Predator</p>
+              <p>Parley</p>
+              <p>Adicolor</p>
+            </div>
+            <div className='support'>
+              <p>SUPPORT</p>
+              <p>Help</p>
+              <p>Customer Services</p>
+              <p>Returns & Exchanges</p>
+              <p>Shipping</p>
+              <p>Order Tracker</p>
+              <p>Store Finder</p>
+              <p>adiClub</p>
+              <p>adiclub Terms and conditions</p>
+            </div>
+            <div className='companyinfo'>
+              <p>COMPANY INFO</p>
+              <p>About Us</p>
+              <p>adidas stories</p>
+              <p>adidas Apps</p>
+              <p>Entity Details</p>
+              <p>Press</p>
+              <p>Careers</p>
+            </div>
+            <div className='folloeus'>
+              <p>FOLLOW US</p>
+              <div className='instagram'><FaInstagram style={{color:"white"}}/></div>
+            </div>
+          </div>
+          <div className='footer2'>
+            <p>Privacy Policy  |  Terms and Conditions  |  Cookies</p>
+            <p>©2024 adidas India Marketing Pvt. Ltd</p>
+          </div>
         </div>
     </div>
   )
