66533f7759c0d045b228aa239d3898b5ccfb92de
Chetan2380 <chetaningale2398@gmail.com>
2025-05-20 16:54:17 +0530

    Update

---
+++ adidas_backend/index.js
@@ -0,0 +1,44 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import AllRoutes from "./routes/index.js"
+import session from 'express-session';
+const app = express();
+app.use(cookieParser());
+app.use(morgan("combined"));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["https://recipe-sharing-platform-sand.vercel.app"],
+  })
+);
+dotenv.config();
+app.use(express.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(session({
+  secret: 'MYSECRETKEY',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: true, sameSite: 'None' }
+}));
+
+
+app.get("/", function (req, res) {
+  res.send("working.");
+});
+
+app.use("/api/v1", AllRoutes);
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("DB connected."));
+
+app.listen(process.env.PORT_NUMBER, () => {
+  console.log(`Server is running on port ${process.env.PORT_NUMBER}.`);
+});
---
+++ adidas_backend/routes/index.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import AuthRoutes from "../routes/auth.routes.js"
+
+const router = Router();
+
+router.use("/auth", AuthRoutes);
+
+export default router;
